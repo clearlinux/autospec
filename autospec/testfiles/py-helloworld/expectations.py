d9802344b364889cc6a2cec3df9069e52fdaddcb
Matthew Johnson <matthew.johnson@intel.com>
2016-11-14 16:42:25 -0800

    Add py-helloworld source to test
    Signed-off-by: Matthew Johnson <matthew.johnson@intel.com>

---
+++ autospec/testfiles/py-helloworld/expectations.py
@@ -0,0 +1,14 @@
+"""
+This file, expectations.py, defines expected test results and is not added to
+the test source tarball.
+"""
+import os
+
+buildreqs = ['pip', 'mock', 'requests', 'six', 'docutils', 'python']
+license = 'GPL-3.0'
+
+with open('testfiles/py-helloworld/spec-expectations', 'r') as spec:
+    specfile = spec.read()
+
+specfile = specfile.replace('{}', os.getcwd()).split('\n')
+output_strings = []
