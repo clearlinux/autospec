81e90d7463f2bb7f6741aceca6555c1cb6814dc1
Arjan van de Ven <arjan@linux.intel.com>
2017-04-04 13:28:39 +0000

    ensure a locales subpackage exists

--- autospec/files.py
+++ autospec/files.py
@@ -93,6 +93,8 @@ class FileManager(object):
             if lang not in self.locales:
                 self.locales.append(lang)
                 print("  New locale:", lang)
+                if  "locales" not in self.packages:
+                    self.packages["locales"] = set()
 
             return True
         else:
