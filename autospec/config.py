de04724f0b1c010aa1e1e84acf8945e46a1382f7
Thiago Macieira <thiago.macieira@intel.com>
2018-08-20 12:47:28 -0700

    Add the ability to make the dev package depend on extras
    Sometimes we move libraries with API to extras, so the dev package
    should cause it to be installed.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- autospec/config.py
+++ autospec/config.py
@@ -119,6 +119,7 @@ config_options = {
     "verify_required": "require package verification for build",
     "security_sensitive": "set flags for security-sensitive builds",
     "so_to_lib": "add .so files to the lib package instead of dev",
+    "dev_requires_extras": "dev package requires the extras to be installed",
     "autoupdate": "this package is trusted enough to automatically update "
                   "(used by other tools)",
     "compat": "this package is a library compatability package and only "
