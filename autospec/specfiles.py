2cd3d3a1fe477a96155a0f957cd0457ee407cd30
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2018-09-25 11:04:14 -0700

    Include --32 in ASFLAGS for 32 bit builds

--- autospec/specfiles.py
+++ autospec/specfiles.py
@@ -432,6 +432,7 @@ class Specfile(object):
 
     def write_32bit_exports(self):
         self._write_strip('export PKG_CONFIG_PATH="/usr/lib32/pkgconfig"')
+        self._write_strip('export ASFLAGS="$ASFLAGS --32"')
         self._write_strip('export CFLAGS="$CFLAGS -m32"')
         self._write_strip('export CXXFLAGS="$CXXFLAGS -m32"')
         self._write_strip('export LDFLAGS="$LDFLAGS -m32"')
