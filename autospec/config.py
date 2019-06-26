30395d4b86e778a7f6d606ab09ffb74bf13a393b
Patrick McCarty <patrick.mccarty@intel.com>
2019-06-26 08:59:38 -0700

    config: case-insensitive adjustment for certain meson patterns
    Fixes recognition of this pattern:
    
      Run-time dependency <INSERT PKGNAME HERE> found: NO (tried pkgconfig)
    
    Also, apply similar treatment to related patterns.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- autospec/config.py
+++ autospec/config.py
@@ -187,9 +187,9 @@ simple_pats = [
 # failed_pattern patterns
 # contains patterns for parsing build.log for missing dependencies
 failed_pats = [
-    (r"Dependency (.*) found: NO \(tried pkgconfig and cmake\)", 0, 'pkgconfig'),
-    (r"Dependency (.*) found: NO \(tried pkgconfig\)", 0, 'pkgconfig'),
-    (r"Dependency (.*) found: NO", 0, None),
+    (r"[Dd]ependency (.*) found: NO \(tried pkgconfig and cmake\)", 0, 'pkgconfig'),
+    (r"[Dd]ependency (.*) found: NO \(tried pkgconfig\)", 0, 'pkgconfig'),
+    (r"[Dd]ependency (.*) found: NO", 0, None),
     (r"C library '(.*)' not found", 0, None),
     (r"Target '[a-zA-Z0-9\-]' can't be generated as '(.*)' could not be found", 0, None),
     (r"Program (.*) found: NO", 0, None),
