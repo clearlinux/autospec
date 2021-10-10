784b4b90ae2a07335acf5d3f7ea891fb374fb1cc
Arjan van de Ven <arjan.van.de.ven@intel.com>
2021-10-10 23:37:50 +0000

    one more file category

--- autospec/files.py
+++ autospec/files.py
@@ -241,6 +241,7 @@ class FileManager(object):
             (r"^/usr/share/clear/optimized-elf/bin", "bin", "/usr/share/clear/optimized-elf/bin*"),
             (r"^/usr/share/clear/optimized-elf/exec", "libexec", "/usr/share/clear/optimized-elf/exec*"),
             (r"^/usr/share/clear/optimized-elf/lib", "lib", "/usr/share/clear/optimized-elf/lib*"),
+            (r"^/usr/share/clear/optimized-elf/other", "lib", "/usr/share/clear/optimized-elf/other*"),
             (r"^/usr/share/clear/optimized-elf/", "lib"),
             (r"^/usr/share/clear/filemap/", "filemap"),
             (r"^/usr/lib64/openmpi/bin/", "openmpi"),
