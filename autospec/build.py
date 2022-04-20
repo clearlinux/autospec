f8689fa6b3a9a9d967936053303bcd7e910cb80f
William Douglas <william.douglas@intel.com>
2022-04-20 12:23:46 -0700

    Enable short-circuit with excludes
    When packages have excludes previously we would full build because
    there was no method to determine if the excludes list had items
    dynamically added to it.
    
    This change adds a new list that is the original exlcudes list
    manually configured and is used to compare against the excludes list
    that is generated during processing the files list. If the two
    lists (compared as sets) are the identical, then short-circuiting is
    still possible.
    
    Also fixup some out of date documentation.
    
    Signed-off-by: William Douglas <william.douglas@intel.com>

--- autospec/build.py
+++ autospec/build.py
@@ -293,7 +293,7 @@ class Build(object):
             mockopts,
         ]
 
-        if not cleanup and self.must_restart == 0 and self.file_restart > 0 and len(filemanager.excludes) == 0:
+        if not cleanup and self.must_restart == 0 and self.file_restart > 0 and set(filemanager.excludes) == set(filemanager.manual_excludes):
             cmd_args.append("--no-clean")
             cmd_args.append("--short-circuit=binary")
 
